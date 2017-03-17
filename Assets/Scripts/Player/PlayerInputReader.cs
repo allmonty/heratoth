820791dc0fc431065eb186dc55ead62a03b835ca
Allan <allanbrados@gmail.com>
2017-03-16 22:33:48 -0300

    Adds a player capable of moving in the scenario

---
+++ Assets/Scripts/Player/PlayerInputReader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputReader : MonoBehaviour {
+
+	[SerializeField] PlayerMovement playerMovement = null;
+
+    float inputHorizontal = 0.0f;
+    float inputVertical = 0.0f;
+
+	void Update () {
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputVertical = Input.GetAxis("Vertical");
+        if(inputHorizontal != 0.0f || inputVertical != 0.0f)
+        {
+            Vector3 moveDirection = new Vector3(inputHorizontal, 0.0f, inputVertical);
+            Quaternion cameraLookDir = Camera.main.transform.rotation;
+            cameraLookDir.x = 0.0f;
+
+            moveDirection = cameraLookDir * moveDirection;
+            playerMovement.moveHorizontal(moveDirection.x, moveDirection.z);
+        }	
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public PlayerMovementController movementController;
+
+	Rigidbody rigid;
+
+	void Start () {
+		rigid = GetComponent<Rigidbody> ();
+	}
+
+	public void moveHorizontal(float dirX, float dirZ) {
+		rigid.velocity = movementController.getHorizontalVelocity(dirX, dirZ, rigid.velocity.y);
+
+        transform.LookAt(transform.position + new Vector3(dirX, 0.0f, dirZ), Vector3.up);
+	}
+}
