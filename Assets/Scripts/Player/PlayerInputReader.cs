e61eab2b8757b49c85c50e0702ca8ca3c672ba1f
Allan <allanbrados@gmail.com>
2017-03-20 20:04:58 -0300

    Refactoring in playerInput script Making the input vector normalized if the magnitude is greater than 1

--- Assets/Scripts/Player/PlayerInputReader.cs
+++ Assets/Scripts/Player/PlayerInputReader.cs
@@ -11,14 +11,24 @@ public class PlayerInputReader : MonoBehaviour {
 	void Update () {
         inputHorizontal = Input.GetAxis("Horizontal");
         inputVertical = Input.GetAxis("Vertical");
+
         if(inputHorizontal != 0.0f || inputVertical != 0.0f)
         {
-            Vector3 moveDirection = new Vector3(inputHorizontal, 0.0f, inputVertical);
-            Quaternion cameraLookDir = Camera.main.transform.rotation;
-            cameraLookDir.x = 0.0f;
+            Vector3 moveDirection = processDirectionInRelationToCamera(inputHorizontal, 0.0f, inputVertical);
+            if(moveDirection.magnitude > 1) moveDirection.Normalize();
 
-            moveDirection = cameraLookDir * moveDirection;
             playerMovement.moveHorizontal(moveDirection.x, moveDirection.z);
-        }	
-	}
+        }
+    }
+
+    private Vector3 processDirectionInRelationToCamera(float x, float y, float z)
+    {
+        Vector3 direction = new Vector3(x, y, z);
+        Quaternion cameraRotation = Camera.main.transform.rotation;
+
+        cameraRotation.x = 0.0f; //removes the vertical rotation
+
+        direction = cameraRotation * direction;
+        return direction;
+    }
 }
