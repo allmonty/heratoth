b9a8b67a6050846753f2a3d733ed7ca371284791
Miguel Péres <foxmpc@gmail.com>
2017-04-10 23:24:30 -0300

    Add Chase Action (script and asset)

---
+++ Assets/Scripts/AI/Enemy/Chase_Action.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu (menuName = "AI/Actions/Chase")]
+public class Chase_Action : Action {
+
+	public override void Act(StateController controller) {
+		Chase(controller);
+	}
+
+	private void Chase(StateController controller) {
+		var enemyControl = controller as Enemy_StateController;
+
+		enemyControl.navMeshAgent.destination = enemyControl.chaseTarget.position;
+		enemyControl.navMeshAgent.Resume();
+	}
+}
