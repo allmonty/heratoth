0796b55ab36b037491b6f57209ab327f74fece2a
Allan <allanbrados@gmail.com>
2017-04-22 21:57:45 -0300

    Changes obsole resume in navMeshAgent for the actual isStopped.

--- Assets/Scripts/Enemy/Chase_Action.cs
+++ Assets/Scripts/Enemy/Chase_Action.cs
@@ -4,13 +4,13 @@
 public class Chase_Action : Action {
 
 	public override void Act(StateController controller) {
-		Chase(controller);
+		chase(controller);
 	}
 
-	private void Chase(StateController controller) {
+	private void chase(StateController controller) {
 		var enemyControl = controller as Enemy_StateController;
 
 		enemyControl.navMeshAgent.destination = enemyControl.chaseTarget.position;
-		enemyControl.navMeshAgent.Resume();
+		enemyControl.navMeshAgent.isStopped = false;
 	}
 }
