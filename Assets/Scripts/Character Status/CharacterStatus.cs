dad0b86e3dddcf85c8d29a1a10c8feff36732011
Allan <allanbrados@gmail.com>
2017-04-22 23:09:27 -0300

    Creates life status.

--- Assets/Scripts/Character Status/CharacterStatus.cs
+++ Assets/Scripts/Character Status/CharacterStatus.cs
@@ -2,9 +2,11 @@
 
 public class CharacterStatus : MonoBehaviour {
 
+	public Status_Life life;
 	public Status_Stamina stamina;
 
 	void Awake() {
+		life.init();
 		stamina.init();
 	}
 
---
+++ Assets/Scripts/Character Status/Status_Life.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Status_Life {
+
+	public float maxLife = 10f;
+	public float life = 10f;
+
+	public void init() {
+		life = maxLife;
+	}
+
+	public bool isEnough(float amount) {
+		if(life >= amount){
+			return true;
+		}
+		return false;
+	}
+
+	public void decrease(float amount) {
+		life -= amount;
+
+		if(life <= 0) {
+            Debug.Log("OUT OF LIFE");
+		}
+	}
+
+	public void increase(float amount) {
+		life += amount;
+		
+		if(life > maxLife){
+			life = maxLife;
+		}
+	}
+}
