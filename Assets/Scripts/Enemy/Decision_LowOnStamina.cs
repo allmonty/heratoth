09dce14aeaa39c96b4a2dc5b49d353836612fb0f
Miguel Péres <foxmpc@gmail.com>
2017-04-27 21:18:48 -0300

    Add LowOnStamina decision, to transition out of the AttackState

---
+++ Assets/Scripts/Enemy/Decision_LowOnStamina.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu (menuName = "AI/Decisions/LowOnStamina")]
+public class Decision_LowOnStamina : Decision {
+
+	[SerializeField] float lowStamina;
+
+	public override bool Decide(StateController controller) {
+		return isLowOnStamina(controller);
+	}
+
+	private bool isLowOnStamina(StateController controller) {
+		Enemy_StateController enemyController = controller as Enemy_StateController;
+
+		var characterStamina = enemyController.characterStatus.stamina;
+		if( characterStamina.stamina <= lowStamina ) {
+			Debug.Log("IS LOW ON STAMINA");
+			return true;
+		}
+
+		Debug.Log("STAMINA OK");
+		return false;
+	}
+}
