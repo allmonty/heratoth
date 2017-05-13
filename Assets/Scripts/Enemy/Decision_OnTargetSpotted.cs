58b908bdc606f3c1ae500d76e04a0b685a02ccc0
Allan <allanbrados@gmail.com>
2017-05-13 14:49:56 -0300

    Decision OnTargetSpotted new uses distance and angle to validate if target was spotted.

--- Assets/Scripts/Enemy/Decision_OnTargetSpotted.cs
+++ Assets/Scripts/Enemy/Decision_OnTargetSpotted.cs
@@ -3,8 +3,11 @@
 [CreateAssetMenu (menuName = "AI/Decisions/TargetSpot")]
 public class Decision_OnTargetSpotted : Decision {
 	
-	[SerializeField] float lookRange;
-	[SerializeField] string targetTag;
+	[SerializeField] float lookRange = 5f;
+	[SerializeField] float lookMaxAngle = 20f;
+	[SerializeField] string targetTag = "Player";
+
+	[SerializeField] Transform target = null;
 
 	public override bool Decide(StateController controller) {
 		return TargetInSight(controller);
@@ -12,18 +15,49 @@ public class Decision_OnTargetSpotted : Decision {
 
 	private bool TargetInSight(StateController controller) {
 		Enemy_StateController control = controller as Enemy_StateController;
+		Transform spotter = control.gameObject.transform;
 		
-		RaycastHit hit;		
-		Debug.DrawRay(control.eyes.position, control.eyes.forward.normalized * lookRange, Color.green);
+		setTarget(control);
 
-		Vector3 origin = control.eyes.position;
-		Vector3 direction = control.eyes.forward;
+		debugVision(control);
 
-		if( Physics.Raycast(origin, direction, out hit) && hit.collider.CompareTag("Player") ) {
-			return true;
-		}
-		else {
+		if(Vector3.Distance(spotter.position, target.position) >= lookRange)
+		{
 			return false;
 		}
+		else
+		{
+			Vector3 targetDir = (target.position - spotter.position).normalized;
+			if(Vector3.Angle(spotter.forward, targetDir) > lookMaxAngle)
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+	}
+
+	private void setTarget(Enemy_StateController control)
+	{
+		if(control.chaseTarget == null)
+		{
+			if(target == null)
+				target = GameObject.FindGameObjectWithTag("Player").transform;
+		}
+		else
+		{
+			target = control.chaseTarget;
+		}
+	}
+
+	private void debugVision(Enemy_StateController control)	
+	{
+		Vector3 dirRight = Quaternion.AngleAxis(lookMaxAngle, Vector3.up) * control.eyes.forward;
+		Vector3 leftRight = Quaternion.AngleAxis(-lookMaxAngle, Vector3.up) * control.eyes.forward;
+
+		Debug.DrawRay(control.eyes.position, dirRight * lookRange, Color.green);
+		Debug.DrawRay(control.eyes.position, leftRight * lookRange, Color.green);
 	}
 }
