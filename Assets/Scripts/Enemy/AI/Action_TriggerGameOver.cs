6b45e4110d05b7bac844db26aa8bb0c6906e1b59
Miguel Péres <foxmpc@gmail.com>
2017-06-07 20:02:55 -0300

    Create GameManager to control game over flow

---
+++ Assets/Scripts/Enemy/AI/Action_TriggerGameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu (menuName = "AI/Actions/TriggerGameOver")]
+public class Action_TriggerGameOver : Action
+{
+	public override void Init(StateController controller) {
+		GameManager.instance.gameOver();
+	}
+
+	public override void Act(StateController controller)
+	{
+		return;
+	}
+	
+	public override void Clear(StateController controller){
+		return;
+	}
+}
---
+++ Assets/Scripts/Utils/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	static public GameManager instance = null;
+
+	public GameObject gameOverDisplay;
+
+	void Awake()
+	{
+		if(instance == null)
+			instance = this;
+		else
+			Destroy(this);
+	}
+
+	public void gameOver() {
+		gameOverDisplay.SetActive(true);
+		StartCoroutine(reloadCurrentScene(5));
+	}
+
+	IEnumerator reloadCurrentScene(float waitTime) {
+		yield return new WaitForSeconds(waitTime);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
