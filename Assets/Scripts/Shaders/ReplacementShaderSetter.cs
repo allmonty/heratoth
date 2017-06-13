58e0decb2101cb0ebe0be86a72fb6a7f3ba31159
Allan <allanbrados@gmail.com>
2017-06-12 22:38:44 -0300

    Using the concepts of XRay Shaders merged with the Unity Standard Shader.

---
+++ Assets/Scripts/Shaders/ReplacementShaderSetter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class ReplacementShaderSetter : MonoBehaviour
+{
+    public Shader XRayShader;
+    public string tag = "XRay";
+
+    void OnEnable()
+    {
+        GetComponent<Camera>().SetReplacementShader(XRayShader, tag);
+    }
+}
