bb22ebb4b3eb56a2239478394337adf6351768bb
Miguel Péres <foxmpc@gmail.com>
2017-09-20 22:16:25 -0300

    Create a tracker object and Log the time player spent reading the first journal

---
+++ Assets/Scripts/OnJournalTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class OnJournalTimer : MonoBehaviour {
+
+	bool tracking = false;
+	float timer = 0f;
+	float totalTime = 0f;
+
+	public void startTimer() {
+		tracking = true;
+		timer += Time.deltaTime;
+	}
+
+	public void stopTimer() {
+		tracking = false;
+		totalTime = timer;
+		timer = 0f;
+	}
+
+	public void sendEvent(string eventName) {
+		Debug.Log("Send event: " + eventName);
+		
+		Analytics.CustomEvent(eventName);
+		
+		// Analytics.CustomEvent(eventName, new Dictionary<string, object>
+		// {
+		// 	{ "Time", totalTime }
+		// });
+	}
+
+	void Update()
+	{
+		if (tracking) {
+			timer += Time.deltaTime;
+		}
+	}
+}
