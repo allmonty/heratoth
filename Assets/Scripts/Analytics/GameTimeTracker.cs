e0ed065b89b1efc683ae14413f655884c025537a
Miguel Péres <foxmpc@gmail.com>
2017-09-26 19:43:53 -0300

    Start GameTimeTracker implementation

---
+++ Assets/Scripts/Analytics/GameTimeTracker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTimeTracker : MonoBehaviour {
+	
+	static public GameTimeTracker instance = null;
+
+	void Awake() {
+		if(instance == null) {
+			instance = this;
+			DontDestroyOnLoad(this);
+		} else {
+			Destroy(this);
+		}
+		
+	}
+}
