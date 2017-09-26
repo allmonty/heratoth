50506b91f83bc85b18dc1ab2cd098ea0aa322db7
Miguel Péres <foxmpc@gmail.com>
2017-09-26 18:49:58 -0300

    Fix journal reading time event

--- Assets/Scripts/OnJournalTimer.cs
+++ Assets/Scripts/OnJournalTimer.cs
@@ -8,40 +8,33 @@ public class OnJournalTimer : MonoBehaviour {
 	bool tracking = false;
 	float timer = 0f;
 	float totalTime = 0f;
-	
-	public string eventName = "teste";
-    public bool execute = false;
 
     public void startTimer() {
 		tracking = true;
 		timer += Time.deltaTime;
 	}
 
-	public void stopTimer() {
-		tracking = false;
+	public void sendEvent() {
 		totalTime = timer;
 		timer = 0f;
-	}
 
-	public void sendEvent(string eventName) {
-		var analyticsStatus = Analytics.CustomEvent(eventName, new Dictionary<string, object>
-		{
-			{ "Time", totalTime }
-		});
+		if (tracking) {
+			var eventName = "Reading Time of " + transform.parent.name;
+			var analyticsStatus = Analytics.CustomEvent(eventName, new Dictionary<string, object>
+			{
+				{ "Reading Time", totalTime }
+			});
 
-		Debug.Log("Event: " + eventName + " | Status: " + analyticsStatus);
+			Debug.Log("Event: " + eventName + " | Status: " + analyticsStatus);
+		}
+
+		tracking = false;
 	}
 
 	void Update()
 	{
 		if (tracking) {
 			timer += Time.deltaTime;
-		}
-
-		if(execute)
-		{
-			sendEvent(eventName);
-			execute = false;
 		}
 	}
 }
