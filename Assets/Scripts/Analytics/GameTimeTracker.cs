3930b5a6ecbbdfbf2a9090835b6868d07f80fa5e
Allan <allanbrados@gmail.com>
2017-09-30 16:22:27 -0300

    Tracking and sending session and runs durations.

--- Assets/Scripts/Analytics/GameTimeTracker.cs
+++ Assets/Scripts/Analytics/GameTimeTracker.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
 
 public class GameTimeTracker : MonoBehaviour {
 	
 	static public GameTimeTracker instance = null;
+
+	List<float> playTimes = new List<float>();
+	float startPlayTime = 0.0f;
 
 	void Awake() {
 		if(instance == null) {
@@ -13,6 +18,56 @@ public class GameTimeTracker : MonoBehaviour {
 		} else {
 			Destroy(this);
 		}
-		
 	}
+
+	void OnEnable(){
+		SceneManager.sceneLoaded += OnLevelLoaded;
+		SceneManager.sceneUnloaded += OnLevelUnloaded;
+	}
+
+	void OnLevelLoaded(Scene scene, LoadSceneMode mode)
+	{
+		if(scene.name.Equals("Game")){
+			startPlayTime = Time.time;
+		}
+	}
+
+	void OnLevelUnloaded(Scene scene)
+	{
+		if(scene.name.Equals("Game")){
+			float runPlayTime = Time.time - startPlayTime;
+			playTimes.Add(runPlayTime);
+		}
+	}
+
+	void OnApplicationQuit(){
+		
+		Debug.Log("esfsa");
+
+		Analytics.CustomEvent("Session Duration", new Dictionary<string, object>
+		{
+			{ "Duration", Time.time }
+		});
+
+		float totalPlayDuration = 0.0f;
+
+		foreach (float duration in playTimes)
+		{
+			Analytics.CustomEvent("Run Duration", new Dictionary<string, object>
+			{
+				{ "Duration", duration }
+			});
+
+			totalPlayDuration += duration;
+		}
+
+		Debug.Log(Analytics.CustomEvent("Total Play Duration", new Dictionary<string, object>
+		{
+			{ "Duration", totalPlayDuration },
+			{ "Runs Count", playTimes.Count }
+		}));
+
+		Debug.Log("cabo");
+	}
+
 }
