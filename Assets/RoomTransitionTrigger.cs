4360663b49f0385a9f197438782223afc42118a0
Allan <allanbrados@gmail.com>
2017-09-30 19:20:10 -0300

    Tracking and sending time in rooms

---
+++ Assets/RoomTransitionTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransitionTrigger : MonoBehaviour {
+
+	[SerializeField] RoomTracker roomTracker = null;
+	[SerializeField] string room1 = "";
+	[SerializeField] string room2 = "";
+
+	 void OnTriggerExit(Collider other){
+		 if(other.gameObject.layer == LayerMask.NameToLayer("Player")){
+		 	roomTracker.walkedThroughDoor(room1, room2);
+		 }
+	 }
+}
---
+++ Assets/Scripts/Analytics/RoomTracker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+
+public class RoomTracker : MonoBehaviour {
+
+	[SerializeField] string currentRoom = "";
+	float timeWhenEnteredRoom = 0.0f;
+
+	void Awake()
+	{
+		timeWhenEnteredRoom = Time.time;
+	}
+
+	public void walkedThroughDoor(string room1, string room2)
+	{
+		string previousRoom = "";
+		if(currentRoom.Equals(room1))
+		{
+			previousRoom = room1;
+			currentRoom = room2;
+		}
+		else
+		{
+			previousRoom = room2;
+			currentRoom = room1;
+		}
+
+		float timeInRoom = Time.time - timeWhenEnteredRoom;
+
+		Debug.Log("Time in Room: " + previousRoom + " " +
+			Analytics.CustomEvent("Time in Room: " + previousRoom, new Dictionary<string, object>
+		{
+			{ "Duration", timeInRoom }
+		}));
+
+		timeWhenEnteredRoom = Time.time;
+	}
+}
