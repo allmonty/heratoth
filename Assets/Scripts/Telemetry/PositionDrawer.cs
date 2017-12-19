e980e3d836ac689cb00df902c8b5c66a4b329019
Allan David <allanbrados@gmail.com>
2017-12-19 19:02:06 -0200

    Creates one position tracker and drawer

---
+++ Assets/Scripts/Telemetry/PositionDrawer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PositionDrawer : MonoBehaviour {
+
+	public bool runDrawer = false;
+
+	List<PlayerPosition> positions = new List<PlayerPosition>();
+
+	void Update() {
+		if(runDrawer)
+		{
+			string path = Application.persistentDataPath + "/playerPosition.json";
+
+			StreamReader reader = new StreamReader(path);
+			
+			PlayerPositions playerPos = JsonUtility.FromJson<PlayerPositions>(reader.ReadToEnd());
+			
+			positions = new List<PlayerPosition>(playerPos.positions);
+
+			reader.Close();
+			runDrawer = false;
+		}
+	}
+	
+	void OnDrawGizmos () {
+		for(int i = 0; i < positions.Count - 1; i++)
+		{
+			Gizmos.color = new Color(1f, 0f, 0f);
+			Gizmos.DrawLine(positions[i].getVector(), positions[i+1].getVector());
+			Gizmos.DrawCube(positions[i].getVector(), new Vector3(1, 1, 1));
+		}
+	}
+}
---
+++ Assets/Scripts/Telemetry/PositionTracker.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class PositionTracker : MonoBehaviour {
+
+	[SerializeField] float trackingDelay = 0f;
+
+	List<Vector3> positionsTracked = new List<Vector3>();
+
+	void Start () {
+		InvokeRepeating("track", 0f, trackingDelay);		
+	}
+
+	void track() {
+		positionsTracked.Add(transform.position);
+	}
+
+	void OnDestroy() {
+        StreamWriter writer = new StreamWriter(Application.persistentDataPath + "/playerPosition.json", true);
+		
+		PlayerPositions data = new PlayerPositions();
+
+		foreach (Vector3 position in positionsTracked)
+		{
+			PlayerPosition playerPos = new PlayerPosition();
+			playerPos.init(position);
+			data.positions.Add(playerPos);
+		}
+
+		writer.Flush();
+        writer.WriteLine(JsonUtility.ToJson(data));
+        writer.Close();
+	}
+}
+
+[System.Serializable]
+public class PlayerPositions {
+	public List<PlayerPosition> positions = new List<PlayerPosition>();
+}
+
+[System.Serializable]
+public class PlayerPosition {
+	public float posX;
+	public float posY;
+	public float posZ;
+
+	public void init(Vector3 pos) {
+		posX = pos.x;
+		posY = pos.y;
+		posZ = pos.z;
+	}
+
+	public Vector3 getVector() {
+		return new Vector3(posX, posY, posZ);
+	}
+}
