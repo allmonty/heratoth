b5d2a62eafbcfbf3080cbf8816f737051f6af782
Miguel Péres <foxmpc@gmail.com>
2018-01-23 23:55:36 -0200

    Read data and render line

--- Assets/Scripts/Telemetry/Telemetry_Visualizer.cs
+++ Assets/Scripts/Telemetry/Telemetry_Visualizer.cs
@@ -4,13 +4,27 @@ using System.Dynamic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Telemetry_Visualizer : MonoBehaviour {
 
+	public string sceneName = "";
+	public bool loadSceneName = true;
 	public bool fetchInfo = false;
+	
+	public int roundsCount = 0;
+	public int selectedRound = 0;
 
 	List<JToken> roundNodes = new List<JToken>(); 
 	bool isReading = false;
+
+	LineRenderer lineRenderer = null;
+
+	void Start() {
+		if(this.loadSceneName)
+			this.sceneName = SceneManager.GetActiveScene().name;
+		this.lineRenderer = transform.GetComponent<LineRenderer>();
+	}
 
 	void Update () {
 		if(fetchInfo) {
@@ -28,19 +42,40 @@ public class Telemetry_Visualizer : MonoBehaviour {
 		}
 	}
 
+
 	void setup(string data) {
 		JObject info = JObject.Parse(data);
-		//Debug.Log(info["-L3-eEUSlMmGZqnsfUXO"].GetType());
 		foreach (var session in info) {
 			foreach (var round in session.Value["Rounds"]) {
-				roundNodes.Add(round["Nodes"]);
+				if (round["Scene Name"].Value<string>() == this.sceneName) {
+					Debug.Log(round);
+					roundNodes.Add(round["Nodes"]);
+				}
 			}
-			//Debug.Log(session.Value["Rounds"][0]["Nodes"]); o/
+
+			roundsCount = roundNodes.Count;
+			//Heroic Line of Master Access!!!
+			// Debug.Log(session.Value["Rounds"][0]["Nodes"]); //o/
 		}
+		Debug.Log(roundNodes);
 	}
 
 	void render() {
+		List<Vector3> positions = new List<Vector3>();
+		foreach(var node in roundNodes[selectedRound]) {
+			Debug.Log(node["Position"]);
+			if(node["Name"].Value<string>() == "Player Position" &&
+				node["Type"].Value<string>() == "Agent"){
+				Vector3 position = new Vector3(
+					node["Position"]["x"].Value<float>(),
+					node["Position"]["y"].Value<float>(),
+					node["Position"]["z"].Value<float>()
+				);
+				positions.Add(position);
 			}
 		}
+
+		lineRenderer.positionCount = positions.Count;
+		lineRenderer.SetPositions(positions.ToArray());
 	}
 }
