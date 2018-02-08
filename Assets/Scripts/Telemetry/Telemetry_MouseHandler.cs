6bff7a9b5408cb066d982ad1cff43a2adab2fb77
Miguel Péres <foxmpc@gmail.com>
2018-02-08 20:19:49 -0200

    Add basic tooltip feature

---
+++ Assets/Scripts/Telemetry/Telemetry_MouseHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Telemetry_MouseHandler : MonoBehaviour {
+
+  [SerializeField] string tagName = "TelemetryNode";
+
+  Telemetry_TooltipHandler currentTooltipHandler = null;
+
+	void OnEnable() {
+		Debug.Log("mouse interaction onenable");
+    SceneView.onSceneGUIDelegate += this.OnSceneMouseOver;
+  }
+
+  void OnSceneMouseOver(SceneView view) {
+    Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+    RaycastHit hit;
+    //And add switch Event.current.type for checking Mouse click and switch tiles
+    if (Physics.Raycast (ray, out hit, 100f)) {
+      if(currentTooltipHandler != null && hit.transform != currentTooltipHandler) {
+        currentTooltipHandler.close();
+      }
+      if(hit.transform.tag.Equals(tagName)) {
+        currentTooltipHandler = hit.transform.GetComponent<Telemetry_TooltipHandler>();
+        currentTooltipHandler.open();
+      }
+    }
+  }
+	
+}
---
+++ Assets/Scripts/Telemetry/Telemetry_TooltipHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telemetry_TooltipHandler : MonoBehaviour {
+
+	[SerializeField] GameObject tooltip = null;
+
+	public void open() {
+		tooltip.SetActive(true);
+	}
+
+	public void close() {
+		tooltip.SetActive(false);
+	}
+}
